ae32f664e355d7461f068752f034841efcff7784
Stephen McMurtry <smcmurtry@gmail.com>
2022-10-26 20:22:56 -0400

    bug fixes

--- components/filter-meetings.js
+++ components/filter-meetings.js
@@ -25,23 +25,23 @@ var start2009 = new Date(2009, 0, 0)
 var start2008 = new Date(2008, 0, 0)
 
 const timeRangeOptions = [
-  {"label": "the last 30 days", "startDate": thirtyDaysAgo, "endDate": today},
-  {"label": "the last 60 days", "startDate": sixtyDaysAgo, "endDate": today},
-  {"label": "2022", "startDate": start2022, "endDate": today},
-  {"label": "2021", "startDate": start2021, "endDate": start2022},
-  {"label": "2020", "startDate": start2020, "endDate": start2021},
-  {"label": "2019", "startDate": start2019, "endDate": start2020},
-  {"label": "2018", "startDate": start2018, "endDate": start2019},
-  {"label": "2017", "startDate": start2017, "endDate": start2018},
-  {"label": "2016", "startDate": start2016, "endDate": start2017},
-  {"label": "2015", "startDate": start2015, "endDate": start2016},
-  {"label": "2014", "startDate": start2014, "endDate": start2015},
-  {"label": "2013", "startDate": start2013, "endDate": start2014},
-  {"label": "2012", "startDate": start2012, "endDate": start2013},
-  {"label": "2011", "startDate": start2011, "endDate": start2012},
-  {"label": "2010", "startDate": start2010, "endDate": start2011},
-  {"label": "2009", "startDate": start2009, "endDate": start2010},
-  {"label": "2008", "startDate": start2008, "endDate": start2009},
+  { "label": "the last 30 days", "startDate": thirtyDaysAgo, "endDate": today },
+  { "label": "the last 60 days", "startDate": sixtyDaysAgo, "endDate": today },
+  { "label": "2022", "startDate": start2022, "endDate": today },
+  { "label": "2021", "startDate": start2021, "endDate": start2022 },
+  { "label": "2020", "startDate": start2020, "endDate": start2021 },
+  { "label": "2019", "startDate": start2019, "endDate": start2020 },
+  { "label": "2018", "startDate": start2018, "endDate": start2019 },
+  { "label": "2017", "startDate": start2017, "endDate": start2018 },
+  { "label": "2016", "startDate": start2016, "endDate": start2017 },
+  { "label": "2015", "startDate": start2015, "endDate": start2016 },
+  { "label": "2014", "startDate": start2014, "endDate": start2015 },
+  { "label": "2013", "startDate": start2013, "endDate": start2014 },
+  { "label": "2012", "startDate": start2012, "endDate": start2013 },
+  { "label": "2011", "startDate": start2011, "endDate": start2012 },
+  { "label": "2010", "startDate": start2010, "endDate": start2011 },
+  { "label": "2009", "startDate": start2009, "endDate": start2010 },
+  { "label": "2008", "startDate": start2008, "endDate": start2009 },
 ]
 
 export async function getStaticProps() {
@@ -58,20 +58,25 @@ meetingTypes.forEach(x => {
   meedingDict[x["meeting_type"]] = x["label"]
 });
 
-export default function FilterMeetings({allMeetingData}) {
+export default function FilterMeetings({ allMeetingData }) {
   const meetingData = getSortedMeetingData();
+  const knownMeetingTypes = meetingTypes.map(x => x.meeting_type);
   meetingData.forEach(x => {
-    x["meeting_label"] = meedingDict[x["meeting_type"]]
+    if (knownMeetingTypes.indexOf(x["meeting_type"]) > -1) {
+      x["meeting_label"] = meedingDict[x["meeting_type"]]
+    } else {
+      x["meeting_label"] = "Other"
+      console.log("new meeeting type", x["meeting_type"])
+    }
     x.datetime = new Date(x.datetime_iso)
   })
-  const meetingOptionsArray = Array.from(new Set(meetingData.map(x => x.meeting_label)))
-  const _meetingOptions = meetingOptionsArray.map(x => 
-    {
-      return {"label": x}
-    });
-  _meetingOptions.sort((a, b) => {
-    const nameA = a.label.toUpperCase(); // ignore upper and lowercase
-    const nameB = b.label.toUpperCase(); // ignore upper and lowercase
+  const meetingOptionsArray = Array.from(new Set(meetingTypes.map(x => x.label)))
+  const _meetingOptions = meetingOptionsArray.map(x => {
+    return { "label": x }
+  });
+  _meetingOptions.sort(({ label: a }, { label: b }) => {
+    const nameA = a.toUpperCase(); // ignore upper and lowercase
+    const nameB = b.toUpperCase(); // ignore upper and lowercase
     if (nameA < nameB) {
       return -1;
     }
@@ -80,52 +85,50 @@ export default function FilterMeetings({allMeetingData}) {
     }
     return 0;
   });
-  const meetingOptions = [{"label": "All"}].concat(_meetingOptions)
+  const meetingOptions = [{ "label": "All", "meeting_type": "All" }].concat(_meetingOptions)
   const [selectedMeetingType, setSelectedMeetingType] = useState("All");
   const [selectedDateRange, setSelectedDateRange] = useState("the last 30 days");
 
 
   const selectedStartDate = timeRangeOptions.filter(x => x.label == selectedDateRange)[0].startDate
   const selectedEndDate = timeRangeOptions.filter(x => x.label == selectedDateRange)[0].endDate
-  console.log("selectedDateRange", selectedDateRange)
-  console.log("selectedStartDate", selectedStartDate)
-  console.log("selectedEndDate", selectedEndDate)
   const _filteredMeetings = selectedMeetingType == "All" ? meetingData : meetingData.filter(x => x.meeting_label == selectedMeetingType)
 
   const filteredMeetings = _filteredMeetings.filter(x => x.datetime > selectedStartDate && x.datetime <= selectedEndDate)
-    return (
-      <div>
-        <div class="mb-4 border-b-2 pt-8 pb-2">
-          <div class="mb-4">
-            <span>See</span>
-            <select 
-              className={styles.fancySelect} 
-              name="meeting-label" 
-              value={selectedMeetingType} 
-              onChange={e => setSelectedMeetingType(e.target.value)}
-            >
-              {meetingOptions.map(({ label }) => (
-                <option value={label}>{label}</option>
-              ))}
-            </select>
-            <span>meetings</span>
-          </div>
-          <div>
-            <span>for </span>
-            <select 
-              name="time-range" 
-              className={styles.fancySelect}
-              value={selectedDateRange} 
-              onChange={e => setSelectedDateRange(e.target.value)}
-            >
-              {timeRangeOptions.map(({ label }) => (
-                <option value={label}>{label}</option>
-              ))}
-            </select>
-          </div>
-          <div class="pt-4">{filteredMeetings.length} results</div>
+  return (
+    <div>
+      <div class="mb-4 border-b-2 pt-8 pb-2">
+        <div class="mb-4">
+          <span>See</span>
+          <select
+            className={styles.fancySelect}
+            name="meeting-label"
+            value={selectedMeetingType}
+            onChange={e => setSelectedMeetingType(e.target.value)}
+          >
+            {meetingOptions.map(({ label }) => (
+              <option value={label}>{label}</option>
+            ))}
+          </select>
+          <span>meetings</span>
         </div>
-        <MeetingList meetingData={filteredMeetings}/>
+        <div>
+          <span>for </span>
+          <select
+            name="time-range"
+            className={styles.fancySelect}
+            value={selectedDateRange}
+            onChange={e => setSelectedDateRange(e.target.value)}
+          >
+            {timeRangeOptions.map(({ label }) => (
+              <option value={label}>{label}</option>
+            ))}
+          </select>
+        </div>
+        <div class="pt-4">{filteredMeetings.length} results</div>
       </div>
+      <MeetingList meetingData={filteredMeetings} />
+    </div>
 
-    )}
+  )
+}
